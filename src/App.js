4783888653b2bd90a2e6fdf527f01baf3f10ac73
Subrat626 <subrat626@gmail.com>
2022-04-26 00:08:24 +0530

    my commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { greet}  from './greet';
+
+function App() {
+  return (
+    <div className="App">
+      <greet/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/greet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const greet = () => {
+  return (
+    <h1>greet</h1>
+  )
+}
+
